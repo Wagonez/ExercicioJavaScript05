0f9cbb4074fd98c391a6f001be110af38596395a
wagonez costa <96398293+Wagonez@users.noreply.github.com>
2025-10-24 16:46:40 -0300

    1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function buscador(){
     var ano = new Date()
     var agora = ano.getFullYear()
-    var dnac=document.getElementById('naci').value
-    reult.innerHTML=(dnac)
+    var nacimento1 = document.getElementById('naci').value
+   document.getElementById('reult').innerHTML=nacimento1 
 }
